f58a097ff8d0a49bcfd41a09476752398a868d80
Sanje Divakaran <sanje.divakaran@gmail.com>
2021-06-03 22:07:37 -0400

    Added axios and wikipedia api

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
 
-    console.log('I run with every render')
+    //console.log(results);
 
     useEffect(() => {
-        console.log("I run after term changes!")
-    }, [term])
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                }
+            });
+
+            setResults(data.query.search);
+        };
+        if (term) {
+            search();
+        }
+    }, [term]);
 
     return (
         <div>
