b1d5fc8ea179dc43e5115eec3e542c85ed2af5c5
Sanje Divakaran <sanje.divakaran@gmail.com>
2021-06-07 23:31:38 -0400

    Finished Dropdown Component

--- src/App.js
+++ src/App.js
@@ -35,14 +35,20 @@ const options = [
 
 export default () => {
   const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
 
   return (
     <div>
-      <Dropdown 
-        options={options}
-        selected={selected}
-        onSelectedChange={setSelected}
-        />
+      <button onClick={() => setShowDropdown(!showDropdown)}>
+        Toggle Dropdown
+      </button>
+      {showDropdown ?
+        <Dropdown 
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        /> : null
+      }
     </div>
   )
 };
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,13 +1,25 @@
 import { render } from '@testing-library/react';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
     const [open, setOpen] = useState(false);
+    const ref = useRef();
 
     useEffect(() => {
-        document.addEventListener('click', () => {
-            
-        })
+        const onBodyClick = (event) => {
+            if (ref.current.contains(event.target)) {
+                return;
+            }
+
+            setOpen(false);
+        }
+
+        document.body.addEventListener('click', onBodyClick, { capture: true });
+
+        return () => {
+            document.body.removeEventListener('click', onBodyClick, { capture: true })
+        };
+
     }, []);
 
     const renderedOptions = options.map((option) => {
@@ -26,7 +38,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     });
 
     return (
-        <div className="ui form">
+        <div ref={ref} className="ui form">
             <div className="field">
                 <label className="label">
                     Select a Colour
