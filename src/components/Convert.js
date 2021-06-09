b492ed31fbea7edf34c204854e6bb200a035bbc0
Sanje Divakaran <sanje.divakaran@gmail.com>
2021-06-08 23:58:37 -0400

    Finished Widget app

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -4,12 +4,24 @@ import axios from 'axios';
 
 const Convert = ({ language, text }) => {
     const [translated, setTranslated] = useState('');
+    const [debouncedText, setDebouncedText] = useState(text);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedText(text);
+        }, 500);
+
+        return () => {
+            clearTimeout(timerId);
+        };
+
+    }, [text]);
 
     useEffect(() => {
         const doTranslation = async () => {
             const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
                 params: {
-                    q: text,
+                    q: debouncedText,
                     target: language.value,
                     key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
                 },
@@ -19,7 +31,7 @@ const Convert = ({ language, text }) => {
         };
 
         doTranslation();
-    }, [language, text]);
+    }, [language, debouncedText]);
     
     return(
         <div>
