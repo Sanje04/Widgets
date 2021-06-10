a2e5fbbb70888473aca2d87985068933d5ce7615
Sanje Divakaran <sanje.divakaran@gmail.com>
2021-06-09 22:13:29 -0400

    Finished Header/Route

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 
 const Route = ({ path, children }) => {
-    return window.location.pathname === path 
+
+    const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+    useEffect(() => {
+        const onLocationChange = () => {
+            setCurrentPath(window.location.pathname);
+        };
+
+        window.addEventListener('popstate', onLocationChange);
+
+        return () => {
+            window.removeEventListener('popstate', onLocationChange);
+        };
+    }, []);
+
+
+    return currentPath === path 
         ? children
         : null;
 };
