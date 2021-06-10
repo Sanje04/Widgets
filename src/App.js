e839100fb767a39bc0ed39da2a8a85745b902dc6
Sanje Divakaran <sanje.divakaran@gmail.com>
2021-06-09 21:44:34 -0400

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
 
 const items = [
   {
@@ -34,11 +35,48 @@ const options = [
   }
 ]
 
-export default () => {
+const showAccordion = () => {
+  if (window.location.pathname === "/") {
+    return <Accordion items={items} />;
+  }
+}
 
+const showList = () => {
+  if (window.location.pathname === "/list") {
+    return <Search />;
+  }
+}
+
+const showDropdown = () => {
+  if (window.location.pathname === "/dropdown") {
+    return <Dropdown />;
+  }
+}
+
+const showTranslate = () => {
+  if (window.location.pathname === "/translate") {
+    return <Translate />;
+  }
+}
+
+
+
+export default () => {
+  const [selected, onSelectedChange] = useState(options[0]);
   return (
     <div>
-      <Translate />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown label="select a color" options={options} selected={selected} onSelectedChange={onSelectedChange}/>
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   )
 };
